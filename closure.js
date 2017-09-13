6cf9f7cab0bc38883e602e83ff676d260b0dff0a
Jimmy <rollingdog@163.com>
2017-09-13 16:45:23 +0800

    Update closure.js

--- closure.js
+++ closure.js
@@ -1,6 +1,6 @@
 //闭包
 
-闭包就是能够读取其他函数内部变量的函数
+//闭包就是能够读取其他函数内部变量的函数
 
 
 　function f1(){
@@ -14,10 +14,10 @@
 　　result(); // 999
   
   
-  f2 就是闭包
+  //f2 就是闭包
   
   
-  它的最大用处有两个，一个是前面提到的可以读取函数内部的变量，另一个就是让这些变量的值始终保持在内存中。
+  //它的最大用处有两个，一个是前面提到的可以读取函数内部的变量，另一个就是让这些变量的值始终保持在内存中。
   
   for(var i=0;i<10;i++){
     setTimeout(function(){
@@ -25,4 +25,24 @@
     },1000)
   }
   
-  打印结果是 输出10次数字 10
+  //打印结果是 输出10次数字 10
+// 匿名函数只是在引用 外部变量i
+
+
+//模拟私有变量
+
+function Counter(start){
+  var count = start
+  retrun {
+    increment:function(){
+      count++
+    },
+    get:function(){
+      return count
+    }
+  }
+}
+
+var foo = Counter(4)
+foo.increment()
+foo.get()
